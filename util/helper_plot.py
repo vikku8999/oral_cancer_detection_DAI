377fc0b1f2ef93cc04737b967136fc8ea2c74e2d
Sulabh Jha <66020896+sulabh007@users.noreply.github.com>
2024-02-16 23:36:49 +0530

    Add files via upload

---
+++ util/helper_plot.py
@@ -0,0 +1,191 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+###-----------------------------------
+### Function to plot Loss Curve
+###-----------------------------------
+
+def plot_torch_hist(hist_df : pd.DataFrame):
+    '''
+    Args:
+      hist_df : pandas Dataframe with five columns
+                First column need to be epoch, 'x' values
+    '''
+    # instantiate figure
+    fig, axes = plt.subplots(1,2 , figsize = (15,6))
+
+    facecolor = 'cyan'
+    fontsize=12
+
+    # Get columns by index to eliminate any column naming error
+    x = "epoch"
+    y1 = "train_loss"
+    y2 = "test_loss"
+    y3 = "train_acc"
+    y4 = "test_acc"
+
+
+    # properties  matplotlib.patch.Patch
+    props = dict(boxstyle='round', facecolor=facecolor, alpha=0.5)
+
+    # Where was min loss
+    best = hist_df[hist_df[y2] == hist_df[y2].min()]
+
+    # pick first axis
+    ax = axes[0]
+
+    # Plot all losses
+    hist_df.plot(x = x, y = [y1,y2], ax = ax)
+
+    # little beautification
+    txtFmt = "Loss: \n  train: {:6.4f}\n   test: {:6.4f}"
+    txtstr = txtFmt.format(hist_df.iloc[-1][y1],
+                           hist_df.iloc[-1][y2]) #text to plot
+
+    # place a text box in upper middle in axes coords
+    ax.text(0.3, 0.95, txtstr,
+            transform=ax.transAxes,
+            fontsize=fontsize,
+            verticalalignment='top',
+            bbox=props)
+
+    # Mark arrow at lowest
+    offset = (best[y2].max() - best[y2].max())/10
+    ax.annotate(f'Min: {best[y2].to_numpy()[0]:6.4f}', # text to print
+                xy=(best[x].to_numpy(), best[y2].to_numpy()[0]), # Arrow start
+                xytext=(best[x].to_numpy()-2, best[y2].to_numpy()[0]+offset), # location of text
+                fontsize=fontsize, va='bottom', ha='right',bbox=props, # beautification of text
+                arrowprops=dict(facecolor=facecolor, shrink=0.05)) # arrow
+
+    # Draw vertical line at best value
+    ax.axvline(x = best[x].to_numpy(),
+               color = 'green',
+               linestyle='-.', lw = 3);
+
+    ax.set_xlabel(x.capitalize())
+    ax.set_ylabel(y1.capitalize())
+    ax.set_title('Errors')
+    ax.grid()
+    ax.legend(loc = 'upper left') # model legend to upper left
+
+    # pick second axis
+    ax = axes[1]
+
+    # Plot accuracy
+    hist_df.plot(x = x, y = [y3, y4], ax = ax)
+
+    # little beautification
+    txtFmt = "Accuracy: \n  train: {:6.4f}\n  test:  {:6.4f}"
+    txtstr = txtFmt.format(hist_df.iloc[-1][y3],
+                           hist_df.iloc[-1][y4]) #text to plot
+
+    # place a text box in lower middle in axes coords
+    ax.text(0.3, 0.2, txtstr,
+            transform=ax.transAxes, fontsize=fontsize,
+            verticalalignment='top', bbox=props)
+
+    # Mark arrow at lowest
+    offset = (best[y4].max() - best[y4].min())/10
+    ax.annotate(f'Best: {best[y4].to_numpy()[0]:6.4f}', # text to print
+                xy=(best[x].to_numpy(), best[y4].to_numpy()[0]), # Arrow start
+                xytext=(best[x].to_numpy()-2, best[y4].to_numpy()[0]-offset), # location of text
+                fontsize=fontsize, va='bottom', ha='right',bbox=props, # beautification of text
+                arrowprops=dict(facecolor=facecolor, shrink=0.05)) # arrow
+
+
+    # Draw a vertical line at best value
+    ax.axvline(x = best[x].to_numpy(),
+               color = 'green',
+               linestyle='-.', lw = 3)
+
+    # Labels
+    ax.set_xlabel(x.capitalize())
+    ax.set_ylabel(y3.capitalize())
+    ax.set_title('Accuracies')
+    ax.grid();
+    ax.legend(loc = 'lower left')
+
+    plt.tight_layout()
+
+
+
+
+    # pick first axis
+    ax = axes[0]
+
+    # Plot all losses
+    hist_df.plot(x = x, y = [y1,y2], ax = ax)
+
+    # little beautification
+    txtFmt = "Loss: \n  train: {:6.4f}\n   test: {:6.4f}"
+    txtstr = txtFmt.format(hist_df.iloc[-1][y1],
+                           hist_df.iloc[-1][y2]) #text to plot
+
+    # place a text box in upper middle in axes coords
+    ax.text(0.3, 0.95, txtstr,
+            transform=ax.transAxes,
+            fontsize=fontsize,
+            verticalalignment='top',
+            bbox=props)
+
+    # Mark arrow at lowest
+    offset = (best[y2].max() - best[y2].max())/10
+    ax.annotate(f'Min: {best[y2].to_numpy()[0]:6.4f}', # text to print
+                xy=(best[x].to_numpy(), best[y2].to_numpy()[0]), # Arrow start
+                xytext=(best[x].to_numpy()-2, best[y2].to_numpy()[0]+offset), # location of text
+                fontsize=fontsize, va='bottom', ha='right',bbox=props, # beautification of text
+                arrowprops=dict(facecolor=facecolor, shrink=0.05)) # arrow
+
+    # Draw vertical line at best value
+    ax.axvline(x = best[x].to_numpy(),
+               color = 'green',
+               linestyle='-.', lw = 3);
+
+    ax.set_xlabel(x.capitalize())
+    ax.set_ylabel(y1.capitalize())
+    ax.set_title('Errors')
+    ax.grid()
+    ax.legend(loc = 'upper left') # model legend to upper left
+
+    # pick second axis
+    ax = axes[1]
+
+    # Plot accuracy
+    hist_df.plot(x = x, y = [y3, y4], ax = ax)
+
+    # little beautification
+    txtFmt = "Accuracy: \n  train: {:6.4f}\n  test:  {:6.4f}"
+    txtstr = txtFmt.format(hist_df.iloc[-1][y3],
+                           hist_df.iloc[-1][y4]) #text to plot
+
+    # place a text box in lower middle in axes coords
+    ax.text(0.3, 0.2, txtstr,
+            transform=ax.transAxes, fontsize=fontsize,
+            verticalalignment='top', bbox=props)
+
+    # Mark arrow at lowest
+    offset = (best[y4].max() - best[y4].min())/10
+    ax.annotate(f'Best: {best[y4].to_numpy()[0]:6.4f}', # text to print
+                xy=(best[x].to_numpy(), best[y4].to_numpy()[0]), # Arrow start
+                xytext=(best[x].to_numpy()-2, best[y4].to_numpy()[0]-offset), # location of text
+                fontsize=fontsize, va='bottom', ha='right',bbox=props, # beautification of text
+                arrowprops=dict(facecolor=facecolor, shrink=0.05)) # arrow
+
+
+    # Draw a vertical line at best value
+    ax.axvline(x = best[x].to_numpy(),
+               color = 'green',
+               linestyle='-.', lw = 3)
+
+    # Labels
+    ax.set_xlabel(x.capitalize())
+    ax.set_ylabel(y3.capitalize())
+    ax.set_title('Accuracies')
+    ax.grid();
+    ax.legend(loc = 'lower left')
+    
+    plt.tight_layout()
+    
+    plt.savefig('/kaggle/working/plot.png')
--- util/model_engine.py
+++ util/model_engine.py
@@ -24,22 +24,66 @@ Contains functions for training and testing a PyTorch model.
 """
 from torchinfo import summary
 from tqdm.auto import tqdm
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
 import math
 import torch.nn as nn
 from torch.nn import functional as F
 import torch.optim as optim
 
+import numpy as np 
+import os
+import logging
+import seaborn as sns
+
+
+class CheckpointSaver:
+    def __init__(self, dirpath, decreasing=True, top_n=5):
+        """
+        dirpath: Directory path where to store all model weights 
+        decreasing: If decreasing is `True`, then lower metric is better
+        top_n: Total number of models to track based on validation metric value
+        """
+        if not os.path.exists(dirpath): os.makedirs(dirpath)
+        self.dirpath = dirpath
+        self.top_n = top_n 
+        self.decreasing = decreasing
+        self.top_model_paths = []
+        self.best_metric_val = np.Inf if decreasing else -np.Inf
+        
+    def __call__(self, model, epoch, metric_val):
+        model_path = os.path.join(self.dirpath, model.__class__.__name__ + f'_epoch{epoch}.pt')
+        save = metric_val<self.best_metric_val if self.decreasing else metric_val>self.best_metric_val
+        if save: 
+            logging.info(f"Current metric value better than {metric_val} better than best {self.best_metric_val}, saving model at {model_path}")
+            self.best_metric_val = metric_val
+            torch.save(model.state_dict(), model_path)
+            self.top_model_paths.append({'path': model_path, 'score': metric_val})
+            self.top_model_paths = sorted(self.top_model_paths, key=lambda o: o['score'], reverse=not self.decreasing)
+        if len(self.top_model_paths)>self.top_n: 
+            self.cleanup()
+            
+    def log_artifact(self, filename, model_path, metric_val):
+        artifact = wandb.Artifact(filename, type='model', metadata={'Validation score': metric_val})
+        artifact.add_file(model_path)
+        wandb.run.log_artifact(artifact)  
+
+    
+    def cleanup(self):
+        to_remove = self.top_model_paths[self.top_n:]
+        logging.info(f"Removing extra models.. {to_remove}")
+        for o in to_remove:
+            os.remove(o['path'])
+        self.top_model_paths = self.top_model_paths[:self.top_n]
+
 
 class Engine():
-
-
 
     def __init__(self,
                  model,
                  loss_fn,
                  optimizer,
                  device,
+                 dirpath,
                  early_stopping = False,
                  config = None):
 
@@ -52,6 +96,8 @@ class Engine():
         self.counter = 0
         self.early_stop = False # type: ignore
         self.best_score = None
+        
+        self.dirpath = dirpath
 
         # Copy your config
         self.config = config
@@ -65,6 +111,7 @@ class Engine():
                 col_names = ["input_size", "output_size", "num_params", "trainable"],
                 col_width = col_width,
                 row_settings = ["var_names"])
+    
 
     def check_early_stop(self,
                    val_loss,
@@ -141,7 +188,7 @@ class Engine():
             # 2. Calculate  and accumulate loss
             loss = self.loss_fn(outputs, y_train)
 
-            train_loss += loss.item()
+            train_loss += loss.item() 
 
             # 3. Optimizer zero grad
             self.optimizer.zero_grad()
@@ -175,7 +222,7 @@ class Engine():
         log_counter = 0
         # Put model in eval mode
         
-        columns=["id", "image", "guess", "truth"]
+        columns=["Id", "Image", "Predicted", "Actual"]
         for digit in range(3):
             columns.append("score_" + str(digit))
         test_table = wandb.Table(columns=columns)
@@ -204,7 +251,7 @@ class Engine():
 
                 # 2. Calculate and accumulate loss
                 loss = self.loss_fn(outputs, y_test)
-                test_loss += loss.item()
+                test_loss += loss.item() 
 
                 # Calculate and accumulate accuracy
                 self.log_test_predictions(X_test, y_test, outputs, y_pred, test_table, log_counter)
@@ -227,10 +274,50 @@ class Engine():
 
         if self.config:
             wandb.log(metrics)
+            
+        
 
         return test_loss, test_acc
 
 
+    def evaluate(self,
+                eval_dataloader):  
+        
+        self.eval_dataloader = eval_dataloader
+        
+        y_ts_true,  y_ts_pred = [], []
+        
+        self.model.eval()  # Set the model to evaluation mode
+        running_loss =  0.0
+        correct_predictions =  0
+        total_predictions =  0
+
+        with torch.no_grad():  # Disable gradient computation
+            for i, data in enumerate(tqdm(self.eval_dataloader, unit='batch')):
+                
+                inputs, labels = data
+                
+                y_ts_true.extend(labels)
+                
+                inputs = inputs.to(self.device)
+                labels = labels.to(self.device)
+
+                # Forward pass
+                outputs = self.model(inputs)
+                loss = self.loss_fn(outputs, labels)
+
+                # Compute the loss and update the running loss
+                running_loss += loss.item() 
+
+                # Compute the number of correct predictions
+                _, y_pred = torch.max(outputs,  1)
+                y_ts_pred.extend(y_pred.data.cpu())
+
+        # Compute the average loss and accuracy
+        avg_loss = running_loss / len(self.eval_dataloader)
+
+        return avg_loss, (y_ts_true, y_ts_pred)
+    
     def train(self,
               train_dataloader,
               test_dataloader,
@@ -243,7 +330,7 @@ class Engine():
         self.train_dataloader = train_dataloader
         self.test_dataloader = test_dataloader
 
-
+        best_val_loss = float('inf')
         # Create empty results dictionary
         results = {"epoch":[],
                 "train_loss": [],
@@ -255,6 +342,8 @@ class Engine():
         # Make sure model on target device
         self.model.to(self.device)
 
+        checkpoint_saver = CheckpointSaver(dirpath=self.dirpath, decreasing=True, top_n=5)
+        
         # Loop through training and testing steps for a number of epochs
         for epoch in range(self.epochs):
             train_loss, train_acc = self.train_step(epoch)
@@ -276,7 +365,7 @@ class Engine():
             results["train_acc"].append(train_acc)
             results["test_acc"].append(test_acc)
 
-
+            checkpoint_saver(self.model, epoch+1, test_loss)
 
             if self.early_stopping:
                 self.check_early_stop(test_loss, delta, verbose, patience)
@@ -284,7 +373,10 @@ class Engine():
                     print("Early Stopping")
                     break
 
-#         # Mark the run as finished
+        # Mark the run as finished
         wandb.finish()
         # Return the filled results at the end of the epochs
+        
         return results
+    
+    
